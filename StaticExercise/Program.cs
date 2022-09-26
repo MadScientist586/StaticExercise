52edc32bd6f64f6af4a3ac1af989cdfc8c52cff3
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-26 12:25:19 -0500

    first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿using StaticExercise;
+Console.WriteLine("Enter a number (fahrenheit):");
+double userInput = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Your number is {TempConverter.FahrenheitToCelsius(userInput)} celsius.");
+
+Console.WriteLine("Enter a number (celsius):");
+double userInput2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Your number is {TempConverter.CelsiusToFahrenheit(userInput2)} fahrenheit.");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double enteredNumber)
+        {
+            double celciusAnswer = enteredNumber - 32 * 5 / 9;
+            return celciusAnswer;
+        }
+        public static double CelsiusToFahrenheit(double enteredNumber2)
+        {
+            double fahrenheitAnswer = enteredNumber2 * 9 / 5 + 32;
+            return fahrenheitAnswer;
+        }
+    }
+}
